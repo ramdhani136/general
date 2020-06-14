ac206bd72697ec9a8c067e55181df19307c104ea
Ilham Ramdhani <ramdhaniit@gmail.com>
2020-06-14 16:38:28 +0700

    API CRUD Divisi

---
+++ app/Http/Controllers/Divisi/DivisiController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\Divisi;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Model\Karyawan\Karyawan;
+use App\Model\Divisi\Divisi;
+use App\Http\Resources\DivisiResource;
+use Symfony\Component\HttpFoundation\Response;
+
+class DivisiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return DivisiResource::collection(Divisi::latest()->get());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data=Divisi::create($request->all());
+        return response(new DivisiResource($data),response::HTTP_CREATED);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Divisi $divisi)
+    {
+        return new DivisiResource($divisi);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Divisi $divisi)
+    {
+        $divisi->update($request->all());
+        return response('Updated',response::HTTP_CREATED);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Divisi $divisi)
+    {
+        $divisi->delete();
+        return response('Deleted',response::HTTP_OK);
+    }
+}
